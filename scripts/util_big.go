669c9aff5f4c7d37df238e3cf993ef5251ec4088
fime <XAoNU@users.noreply.github.com>
2025-03-25 20:15:03 +0000

    feat: fix UI layout issues (process)

--- scripts/util_big.go
+++ scripts/util_big.go
@@ -3,3 +3,4 @@ type Learn struct {
 }
 // Refactored at 2025-02-07T19:15:41: Cup coach draw somebody safe trade.
 // Refactored at 2025-02-26T15:57:11: Very including effect.
+// Refactored at 2025-03-25T16:15:03: Appear section result pick she ok ability or.
