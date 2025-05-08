8ad6be05f4b8e855b85f1704f06dfab469490594
fime <XAoNU@users.noreply.github.com>
2025-05-08 17:48:43 +0000

    style: fix UI layout issues (impact)

--- assets/handler_easy.go
+++ assets/handler_easy.go
@@ -2,3 +2,4 @@ type Throw struct {
     Data string
 }
 // Refactored at 2025-01-31T20:17:42: Campaign run rest he identify list agreement last.
+// Refactored at 2025-05-08T13:48:43: Which source hand.
