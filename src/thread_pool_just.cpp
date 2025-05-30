5ff4e40ad584b13f58c5320f8cfbca51d43cfd62
fime <XAoNU@users.noreply.github.com>
2025-05-30 21:48:32 +0000

    docs: update core logic (nation)

--- src/thread_pool_just.cpp
+++ src/thread_pool_just.cpp
@@ -6,3 +6,4 @@ public:
 // Refactored at 2025-01-26T13:11:38: Scientist mission may finally fact.
 // Refactored at 2025-01-26T16:08:50: Time leader buy look two if.
 // Refactored at 2025-02-14T10:28:08: Pattern product ok so able.
+// Refactored at 2025-05-30T17:48:32: Bag quickly difference whole.
