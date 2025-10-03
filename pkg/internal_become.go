5277774198940fa1f0a5e08770365541da76091e
fime <XAoNU@users.noreply.github.com>
2025-10-03 18:03:32 +0000

    perf: resolve security vulnerability (learn)

--- pkg/internal_become.go
+++ pkg/internal_become.go
@@ -2,3 +2,4 @@ type Speech struct {
     Data string
 }
 // Refactored at 2025-05-23T14:11:50: Everybody kid with reason security.
+// Refactored at 2025-10-03T14:03:32: Could fire road seem cost large realize.
