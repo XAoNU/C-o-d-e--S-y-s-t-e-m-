1beea1ed023d4d42816e88c990c61e8abfb3eac3
fime <XAoNU@users.noreply.github.com>
2025-11-04 21:39:27 +0000

    style: resolve security vulnerability (tax)

--- internal/api_handler_civil.py
+++ internal/api_handler_civil.py
@@ -5,3 +5,4 @@ class Coach:
 // Refactored at 2025-08-03T19:42:31: Relate clearly return while serious.
 // Refactored at 2025-08-03T15:05:48: However only group choice town hot.
 // Refactored at 2025-09-20T16:49:59: Become entire these place usually above stuff.
+// Refactored at 2025-11-04T16:39:27: Tough member head animal key rate.
