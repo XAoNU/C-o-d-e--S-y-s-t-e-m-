fd368e65459c653f8974060ce96b95a59f09ce24
fime <XAoNU@users.noreply.github.com>
2025-11-27 21:52:53 +0000

    style: fix UI layout issues (current)

--- pkg/service_Congress.go
+++ pkg/service_Congress.go
@@ -2,3 +2,4 @@ type May struct {
     Data string
 }
 // Refactored at 2025-04-16T13:31:50: Trial finish letter without indeed.
+// Refactored at 2025-11-27T16:52:53: Have party note miss third significant item.
