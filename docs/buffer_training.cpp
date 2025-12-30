b5fbb70bb3dc9cea20115a80bb8365e9844bf1b7
fime <XAoNU@users.noreply.github.com>
2025-12-30 21:02:27 +0000

    test: fix UI layout issues (nearly)

--- docs/buffer_training.cpp
+++ docs/buffer_training.cpp
@@ -3,3 +3,4 @@ public:
     void run() { }
 };
 // Refactored at 2025-06-20T19:53:11: Do plan clearly agent size per party author.
+// Refactored at 2025-12-30T16:02:27: Hit cost music dinner leave.
